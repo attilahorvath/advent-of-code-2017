b08b2cff1dc7517d32f21eb5e25fde39b628d805
Attila Horváth <hun.ati500@gmail.com>
2017-12-02 10:08:22 +0000

    Add solution for first part of the day 2 challenge

---
+++ day02/src/main.rs
@@ -0,0 +1,23 @@
+extern crate day02;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use day02::*;
+
+fn main() {
+    let file = BufReader::new(File::open("input.txt").expect("file not found"));
+    let mut rows: Vec<Vec<u32>> = Vec::new();
+
+    for line in file.lines() {
+        let line = line.expect("error reading file");
+
+        rows.push(
+            line.split_whitespace()
+                .map(|i| i.parse().unwrap_or(0))
+                .collect(),
+        );
+    }
+
+    println!("Checksum: {}", calculate_checksum(&rows));
+}
