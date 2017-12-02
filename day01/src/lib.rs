861ada478f4fe812a9f868ae5a8ead8b88a73f7e
Attila Horváth <hun.ati500@gmail.com>
2017-12-02 01:43:37 +0000

    Add solution for second part of day 1 challenge

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -13,6 +13,24 @@ pub fn solve_captcha(captcha: &str) -> u32 {
         .sum()
 }
 
+pub fn solve_second_captcha(captcha: &str) -> u32 {
+    captcha
+        .chars()
+        .enumerate()
+        .map(|(index, c)| if c ==
+            captcha
+                .chars()
+                .cycle()
+                .nth(captcha.len() / 2 + index)
+                .unwrap()
+        {
+            c.to_digit(10).unwrap_or(0)
+        } else {
+            0
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +53,30 @@ mod tests {
     #[test]
     fn last_digit_matches() {
         assert_eq!(9, solve_captcha("91212129"));
+    }
+
+    #[test]
+    fn second_captcha_all_digit_matches() {
+        assert_eq!(6, solve_second_captcha("1212"));
+    }
+
+    #[test]
+    fn second_captcha_no_matches() {
+        assert_eq!(0, solve_second_captcha("1221"));
+    }
+
+    #[test]
+    fn second_captcha_two_matches() {
+        assert_eq!(4, solve_second_captcha("123425"));
+    }
+
+    #[test]
+    fn second_captcha_three_matches() {
+        assert_eq!(12, solve_second_captcha("123123"));
+    }
+
+    #[test]
+    fn second_captcha_ones_match() {
+        assert_eq!(4, solve_second_captcha("12131415"));
     }
 }
