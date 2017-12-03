9f5b98a304c7ee67f443da564604bbc682b1062c
Attila Horváth <hun.ati500@gmail.com>
2017-12-03 21:27:57 +0000

    Add solution for second part of day 3 challenge

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,6 +2,9 @@ extern crate day03;
 
 use day03::*;
 
+const INPUT: u32 = 361527;
+
 fn main() {
-    println!("Distance: {}", distance(361527));
+    println!("Distance: {}", distance(INPUT));
+    println!("Greater value: {}", value_greater_than(INPUT));
 }
