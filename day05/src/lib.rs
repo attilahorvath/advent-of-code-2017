469bcc1e818ee812cd683042adb1b16fd454065b
Attila Horváth <ahorvath@fusioneer.com>
2017-12-05 13:08:48 +0000

    Add solution for second part of day 5 challenge

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -4,8 +4,27 @@ pub fn number_of_steps(offsets: &mut [i32]) -> u32 {
 
     while index >= 0 && index < offsets.len() as i32 {
         steps += 1;
+
         let new_index = index + offsets[index as usize];
         offsets[index as usize] += 1;
+
+        index = new_index;
+    }
+
+    steps
+}
+
+pub fn number_of_steps_with_decrease(offsets: &mut [i32]) -> u32 {
+    let mut index = 0;
+    let mut steps = 0;
+
+    while index >= 0 && index < offsets.len() as i32 {
+        steps += 1;
+
+        let new_index = index + offsets[index as usize];
+        let offset = &mut offsets[index as usize];
+        *offset += if *offset >= 3 { -1 } else { 1 };
+
         index = new_index;
     }
 
@@ -18,6 +37,15 @@ mod tests {
 
     #[test]
     fn count_number_of_steps() {
-        assert_eq!(5, number_of_steps(&mut vec![0, 3, 0, 1, -3]));
+        let mut offsets = vec![0, 3, 0, 1, -3];
+        assert_eq!(5, number_of_steps(&mut offsets));
+        assert_eq!(vec![2, 5, 0, 1, -2], offsets);
+    }
+
+    #[test]
+    fn count_number_of_steps_with_decrease() {
+        let mut offsets = vec![0, 3, 0, 1, -3];
+        assert_eq!(10, number_of_steps_with_decrease(&mut offsets));
+        assert_eq!(vec![2, 3, 2, 3, -1], offsets);
     }
 }
--- day05/src/main.rs
+++ day05/src/main.rs
@@ -7,9 +7,14 @@ use day05::*;
 
 fn main() {
     let file = BufReader::new(File::open("input.txt").expect("file not found"));
-    let mut offsets = file.lines()
+    let offsets = file.lines()
         .map(|i| i.expect("error reading file").parse().unwrap_or(0))
         .collect::<Vec<_>>();
 
-    println!("Number of steps: {}", number_of_steps(&mut offsets));
+    println!("Number of steps: {}", number_of_steps(&mut offsets.clone()));
+
+    println!(
+        "Number of steps with decrease: {}",
+        number_of_steps_with_decrease(&mut offsets.clone())
+    );
 }
