295381dbbaa75908161ad5b067819c9f88159e10
Attila Horváth <hun.ati500@gmail.com>
2017-12-10 00:48:47 +0000

    Add solution for second part of day 9 challenge

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,31 +1,60 @@
-pub fn parse_score(stream: &str) -> u32 {
+use std::ops::AddAssign;
+
+#[derive(Debug, PartialEq)]
+pub struct StreamData {
+    pub score: u32,
+    pub garbage: u32,
+}
+
+impl StreamData {
+    fn new(score: u32, garbage: u32) -> Self {
+        StreamData { score, garbage }
+    }
+}
+
+impl AddAssign for StreamData {
+    fn add_assign(&mut self, other: Self) {
+        self.score += other.score;
+        self.garbage += other.garbage;
+    }
+}
+
+pub fn parse_stream(stream: &str) -> StreamData {
     parse_group(&mut stream.chars(), 0)
 }
 
-fn parse_group(stream: &mut Iterator<Item = char>, base_score: u32) -> u32 {
-    let mut score = base_score;
+fn parse_group(stream: &mut Iterator<Item = char>, base_score: u32) -> StreamData {
+    let mut stream_data = StreamData::new(base_score, 0);
 
     while let Some(c) = stream.next() {
         match c {
-            '{' => score += parse_group(stream, base_score + 1),
-            '}' => return score,
-            '<' => parse_garbage(stream),
-            '!' => { stream.next(); },
+            '{' => stream_data += parse_group(stream, base_score + 1),
+            '}' => break,
+            '<' => stream_data += parse_garbage(stream),
+            '!' => {
+                stream.next();
+            }
             _ => (),
         }
     }
 
-    score
+    stream_data
 }
 
-fn parse_garbage(stream: &mut Iterator<Item = char>) {
+fn parse_garbage(stream: &mut Iterator<Item = char>) -> StreamData {
+    let mut stream_data = StreamData::new(0, 0);
+
     while let Some(c) = stream.next() {
         match c {
-            '>' => return,
-            '!' => { stream.next(); },
-            _ => (),
+            '>' => break,
+            '!' => {
+                stream.next();
+            }
+            _ => stream_data.garbage += 1,
         }
     }
+
+    stream_data
 }
 
 #[cfg(test)]
@@ -34,46 +63,55 @@ mod tests {
 
     #[test]
     fn parse_basic_group() {
-        assert_eq!(1, parse_score("{}"));
+        assert_eq!(StreamData::new(1, 0), parse_stream("{}"));
     }
 
     #[test]
     fn parse_nested_groups() {
-        assert_eq!(6, parse_score("{{{}}}"));
+        assert_eq!(StreamData::new(6, 0), parse_stream("{{{}}}"));
     }
 
     #[test]
     fn parse_two_nested_groups() {
-        assert_eq!(5, parse_score("{{},{}}"));
+        assert_eq!(StreamData::new(5, 0), parse_stream("{{},{}}"));
     }
 
     #[test]
     fn parse_complex_groups() {
-        assert_eq!(16, parse_score("{{{},{},{{}}}}"));
+        assert_eq!(StreamData::new(16, 0), parse_stream("{{{},{},{{}}}}"));
     }
 
     #[test]
     fn parse_garbage() {
-        assert_eq!(1, parse_score("{<a>,<a>,<a>,<a>}"));
+        assert_eq!(StreamData::new(1, 4), parse_stream("{<a>,<a>,<a>,<a>}"));
     }
 
     #[test]
     fn parse_misleading_garbage() {
-        assert_eq!(1, parse_score("{<{},{},{{}}>}"));
+        assert_eq!(StreamData::new(1, 10), parse_stream("{<{},{},{{}}>}"));
     }
 
     #[test]
     fn parse_garbage_groups() {
-        assert_eq!(9, parse_score("{{<ab>},{<ab>},{<ab>},{<ab>}}"));
+        assert_eq!(
+            StreamData::new(9, 8),
+            parse_stream("{{<ab>},{<ab>},{<ab>},{<ab>}}")
+        );
     }
 
     #[test]
     fn parse_canceled_groups() {
-        assert_eq!(9, parse_score("{{<!!>},{<!!>},{<!!>},{<!!>}}"));
+        assert_eq!(
+            StreamData::new(9, 0),
+            parse_stream("{{<!!>},{<!!>},{<!!>},{<!!>}}")
+        );
     }
 
     #[test]
     fn parse_canceled_garbage() {
-        assert_eq!(3, parse_score("{{<a!>},{<a!>},{<a!>},{<ab>}}"));
+        assert_eq!(
+            StreamData::new(3, 17),
+            parse_stream("{{<a!>},{<a!>},{<a!>},{<ab>}}")
+        );
     }
 }
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -7,7 +7,12 @@ use day09::*;
 
 fn main() {
     let file = BufReader::new(File::open("input.txt").expect("file not found"));
-    let stream = file.lines().next().expect("no stream given").expect("error reading file");
+    let stream = file.lines().next().expect("no stream given").expect(
+        "error reading file",
+    );
 
-    println!("Total score: {}", parse_score(&stream));
+    let stream_data = parse_stream(&stream);
+
+    println!("Total score: {}", stream_data.score);
+    println!("Garbage amount: {}", stream_data.garbage);
 }
