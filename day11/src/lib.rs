8b1496ebf0124eba4f26683793683b790303456a
Attila Horváth <hun.ati500@gmail.com>
2017-12-12 00:15:29 +0000

    Add solution for first part of day 11 challenge

---
+++ day11/src/lib.rs
@@ -0,0 +1,133 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+pub enum Direction {
+    N,
+    NE,
+    SE,
+    S,
+    SW,
+    NW,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ParseDirectionError;
+
+impl FromStr for Direction {
+    type Err = ParseDirectionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim() {
+            "n" => Ok(Direction::N),
+            "ne" => Ok(Direction::NE),
+            "se" => Ok(Direction::SE),
+            "s" => Ok(Direction::S),
+            "sw" => Ok(Direction::SW),
+            "nw" => Ok(Direction::NW),
+            _ => Err(ParseDirectionError),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct HexCoords {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl HexCoords {
+    pub fn new() -> Self {
+        HexCoords { x: 0, y: 0, z: 0 }
+    }
+
+    pub fn take_step(&mut self, step: Direction) {
+        match step {
+            Direction::N => {
+                self.y += 1;
+                self.z -= 1;
+            }
+            Direction::NE => {
+                self.x += 1;
+                self.z -= 1;
+            }
+            Direction::SE => {
+                self.x += 1;
+                self.y -= 1;
+            }
+            Direction::S => {
+                self.y -= 1;
+                self.z += 1;
+            }
+            Direction::SW => {
+                self.x -= 1;
+                self.z += 1;
+            }
+            Direction::NW => {
+                self.x -= 1;
+                self.y += 1;
+            }
+        }
+    }
+
+    pub fn distance_from_origin(&self) -> u32 {
+        [self.x, self.y, self.z]
+            .iter()
+            .map(|i| i.abs() as u32)
+            .max()
+            .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn going_northeast() {
+        let mut coords = HexCoords::new();
+
+        coords.take_step(Direction::NE);
+        coords.take_step(Direction::NE);
+        coords.take_step(Direction::NE);
+
+        assert_eq!(3, coords.distance_from_origin());
+    }
+
+    #[test]
+    fn going_northeast_then_coming_back() {
+        let mut coords = HexCoords::new();
+
+        coords.take_step(Direction::NE);
+        coords.take_step(Direction::NE);
+        coords.take_step(Direction::SW);
+        coords.take_step(Direction::SW);
+
+        assert_eq!(0, coords.distance_from_origin());
+    }
+
+    #[test]
+    fn going_northeast_then_south() {
+        let mut coords = HexCoords::new();
+
+        coords.take_step(Direction::NE);
+        coords.take_step(Direction::NE);
+        coords.take_step(Direction::S);
+        coords.take_step(Direction::S);
+
+        assert_eq!(2, coords.distance_from_origin());
+    }
+
+    #[test]
+    fn going_southeast_and_southwest() {
+        let mut coords = HexCoords::new();
+
+        coords.take_step(Direction::SE);
+        coords.take_step(Direction::SW);
+        coords.take_step(Direction::SE);
+        coords.take_step(Direction::SW);
+        coords.take_step(Direction::SW);
+
+        assert_eq!(3, coords.distance_from_origin());
+    }
+}
