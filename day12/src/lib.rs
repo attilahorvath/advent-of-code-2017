944414b37f9c389519872d5a7d7befed32af9bba
Attila Horváth <ahorvath@fusioneer.com>
2017-12-12 12:32:13 +0000

    Add solution for first part of day 12 challenge

---
+++ day12/src/lib.rs
@@ -0,0 +1,108 @@
+use std::collections::{HashMap, HashSet};
+
+pub struct Graph {
+    nodes: HashMap<u32, Node>,
+}
+
+struct Node {
+    neighbours: HashSet<u32>,
+}
+
+impl Graph {
+    pub fn new() -> Self {
+        Graph { nodes: HashMap::new() }
+    }
+
+    pub fn parse_node(&mut self, s: &str) {
+        let mut iter = s.split_whitespace();
+
+        let index = iter.next()
+            .expect("invalid node definition")
+            .parse()
+            .expect("invalid node index");
+
+        iter.next();
+
+        let mut neighbours = Vec::new();
+
+        while let Some(i) = iter.next() {
+            let n = i.trim_matches(',').parse().expect("invalid neighbour data");
+            neighbours.push(n);
+        }
+
+        self.add_node(index, &neighbours);
+    }
+
+    pub fn add_node(&mut self, index: u32, neighbours: &[u32]) {
+        for &n in neighbours {
+            self.link_nodes(index, n);
+        }
+    }
+
+    fn link_nodes(&mut self, a: u32, b: u32) {
+        if a == b {
+            return;
+        }
+
+        {
+            let node = self.nodes.entry(a).or_insert(Node::new());
+            (*node).neighbours.insert(b);
+        }
+
+        {
+            let node = self.nodes.entry(b).or_insert(Node::new());
+            (*node).neighbours.insert(a);
+        }
+    }
+
+    pub fn nodes_in_group(&self, group: u32) -> Vec<u32> {
+        let mut visited = HashSet::new();
+
+        self.visit_nodes_from(group, &mut visited);
+
+        visited.iter().cloned().collect()
+    }
+
+    fn visit_nodes_from(&self, index: u32, visited: &mut HashSet<u32>) {
+        if visited.contains(&index) {
+            return;
+        }
+
+        visited.insert(index);
+
+        let node = self.nodes.get(&index).unwrap();
+
+        for &n in &node.neighbours {
+            self.visit_nodes_from(n, visited);
+        }
+    }
+}
+
+impl Node {
+    fn new() -> Self {
+        Node { neighbours: HashSet::new() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_nodes_in_group() {
+        let mut graph = Graph::new();
+
+        graph.add_node(0, &[2]);
+        graph.add_node(1, &[1]);
+        graph.add_node(2, &[0, 3, 4]);
+        graph.add_node(3, &[2, 4]);
+        graph.add_node(4, &[2, 3, 6]);
+        graph.add_node(5, &[6]);
+        graph.add_node(6, &[4, 5]);
+
+        let mut group = graph.nodes_in_group(0);
+        group.sort();
+
+        assert_eq!(vec![0, 2, 3, 4, 5, 6], group);
+    }
+}
---
+++ day12/src/main.rs
@@ -0,0 +1,18 @@
+extern crate day12;
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use day12::*;
+
+fn main() {
+    let file = BufReader::new(File::open("input.txt").expect("file not found"));
+    let mut graph = Graph::new();
+
+    for l in file.lines() {
+        let l = l.expect("error reading file");
+        graph.parse_node(&l);
+    }
+
+    println!("Nodes in group 0: {}", graph.nodes_in_group(0).len());
+}
